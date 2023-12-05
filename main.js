70ed0f3aca4ceb2dbb4d14187036277971ffe80f
unknown <gozainek@gmail.com>
2023-12-04 23:28:00 -0300

    Mi segundo commit

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    document.getElementById("Envio").addEventListener("click", function(){
+        alert("¡Se ha enviado tu Mensaje, en breve te responderemos!);
+    })
+
+})
---
+++ sweetAlert.js
@@ -0,0 +1,7 @@
+Swal.fire({
+
+    title: "Tu mensaje a sido enviado!",
+    text: "A la brevedad te responderemos!",
+    icon: "success"
+
+});
